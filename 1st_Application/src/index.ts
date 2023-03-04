6d240b45a6833e081322569719b06601cc7d628d
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-03-04 14:15:37 +0530

    funtions

--- 1st_Application/src/index.ts
+++ 1st_Application/src/index.ts
@@ -20,3 +20,8 @@ let nameArray: string[] = [10,20,30,40,"50"]//number එකේ string value ත�
 
 //Tuples
 let person: [number,string] = [1,"Kamal"]//Data type 2k එක පාරට දානවනම් use කරන්නේ
+
+//functions
+function calculator(){
+    return 0;
+}
