8b7b1f23e2c42c341b326b94ae34e3b054baa175
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-03-04 14:12:14 +0530

    interducing tuple

--- 1st_Application/src/index.ts
+++ 1st_Application/src/index.ts
@@ -17,3 +17,6 @@ let numberArray = [10,20,30,40,"50"]
 let numberArray2: number[] = [10,20,30,40,"50"]//number එකේ string value තින නිසා error "50"
 let nameArray: string[] = [10,20,30,40,"50"]//number එකේ string value තින නිසා error "10,20,30,40"
 */
+
+//Tuples
+let person: [number,string] = [1,"Kamal"]//Data type 2k එක පාරට දානවනම් use කරන්නේ
