af778fccdef422fee42c2cdf65cc3d1af44688e1
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-03-04 13:21:09 +0530

    Update project

--- 1st_Application/dist/index.js
+++ 1st_Application/dist/index.js
@@ -6,5 +6,9 @@ let age = 22;
 //String
 let firstName = 'Kamal';
 //Array
-let numbersArray; //any -onama type ekak
+let numbersArray; //any -ඔනම type එකක්
 let numberArray = [10, 20, 30, 40, "50"];
+/**
+let numberArray2: number[] = [10,20,30,40,"50"]//number එකේ string value තින නිසා error "50"
+let nameArray: string[] = [10,20,30,40,"50"]//number එකේ string value තින නිසා error "10,20,30,40"
+*/ 
