82ed18ba99eeb368d29e1ba671785755cfc3cc91
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-03-11 10:58:10 +0530

    Interducing react lfe cycle method componentDidMount()

--- testing-app/src/App.tsx
+++ testing-app/src/App.tsx
@@ -1,15 +1,19 @@
 /** @format */
 
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import logo2 from "./logo.svg";
 import "./App.css";
 
 export default class App extends Component {
+ componentDidMount(): void {
+  console.log("Hi HHi")
+ }
+
   render(): React.ReactNode {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo2} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
