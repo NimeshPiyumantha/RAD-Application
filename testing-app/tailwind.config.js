fb8ed2ef63bfff7ae76c30fb85123086fbc55c82
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-03-11 13:30:04 +0530

    Update project

--- testing-app/tailwind.config.js
+++ testing-app/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        ".src/**/*.{js,jsx,ts,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
