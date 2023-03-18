dfd31aa69422d6c1708cc690c22b8a917cd87db4
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-18 11:25:35 +0530

    Add Decrease button

--- testing-app/src/components/Counter/Counter.tsx
+++ testing-app/src/components/Counter/Counter.tsx
@@ -7,6 +7,7 @@ export default class Counter extends Component {
             <h1>Counter : <span>0</span></h1>
             <div className='flex space-x-3 mt-5'>
                 <button className='border border-green-600 px-4 py-2 text-green-600 font-semibold rounded-xl hover:bg-green-600 hover:text-white'>Increase</button>
+                <button className='border border-red-600 px-4 py-2 text-red-600 font-semibold rounded-xl hover:bg-red-600 hover:text-white'>Decrease</button>
             </div>
         </div>
     )
