e144503e4c269932dcb7c212ee727cac3a49929b
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-18 16:39:12 +0530

    Update Content

--- testing-app/src/App.tsx
+++ testing-app/src/App.tsx
@@ -5,10 +5,6 @@ import React, { Component } from "react";
 import "./App.css";
 import Content from "./components/Content";
 import Header from "./components/Header";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
 
 export default class App extends Component {
   render(): React.ReactNode {
--- testing-app/src/components/Header/Header.tsx
+++ testing-app/src/components/Header/Header.tsx
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import userImage from "../../assets/user.png";
 
 export default class Header extends Component {
   render() {
+    const activeLink = "border-b-2 cursor-pointer";
+    const normalLink = "cursor-pointer";
+
     return (
       <header>
-        <div className="bg-white border-gray-200 px-4 h-20 lg:px-6 py-2.5 dark:bg-gray-600 font-Poppins">
+        <div className="bg-white border-gray-200 px-4 h-16  dark:bg-gray-600 font-Poppins">
           <div className="w-full h-full">
             <div className="flex w-full h-full items-center">
-              <div className="w-3/4 flex items-center">
-                <h1 className="font-Poppins">Nimma</h1>
+              <div className="w-3/4 flex justify-center">
+                <h1 className="font-Poppins ">Nimma</h1>
               </div>
               <div className="w-1/2">
-              
                 <nav className="flex w-full gap-5">
-                  <Link to={"/"}><h4>Home</h4></Link>
-                  <Link to={"/about"}><h4>About</h4></Link>
-                  <Link to={"/contact"}><h4>Contact</h4></Link>
-                  <Link to={"/profile"}>
-                    <img src={userImage} alt="userImage" className="w-12 h-12" />
-                  </Link>
+                  <NavLink to={"/"} className={({isActive})=>(isActive ? activeLink: normalLink)}>
+                    <h4>Home</h4>
+                  </NavLink>
+                  <NavLink to={"/about"} className={({isActive})=>(isActive ? activeLink: normalLink)}>
+                    <h4>About</h4>
+                  </NavLink>
+                  <NavLink to={"/contact"} className={({isActive})=>(isActive ? activeLink: normalLink)}>
+                    <h4>Contact</h4>
+                  </NavLink>
+                  <NavLink to={"/profile"}>
+                    <img
+                      src={userImage}
+                      alt="userImage"
+                      className="w-8 h-8"
+                    />
+                  </NavLink>
                 </nav>
-              
               </div>
             </div>
           </div>
