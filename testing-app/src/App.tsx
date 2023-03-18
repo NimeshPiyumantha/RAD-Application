ff12a3b8dc47341e35346cd088f9dca57e759ce7
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-18 09:13:10 +0530

    Update Content

--- testing-app/src/App.tsx
+++ testing-app/src/App.tsx
@@ -11,7 +11,7 @@ export default class App extends Component {
 
   render(): React.ReactNode {
     return (
-      <div className="App bg-accent-navy-100 min-h-screen">
+      <div className="App bg-accent-navy-200 min-h-screen">
         <h1>Hi</h1>
         <h2>Hidid</h2>
       </div>
