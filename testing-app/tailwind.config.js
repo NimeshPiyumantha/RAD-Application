e1827acf15e57aa50c7f557a2fb49a740ed852cb
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-18 10:46:12 +0530

    Update Content

--- testing-app/tailwind.config.js
+++ testing-app/tailwind.config.js
@@ -2,25 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"
 ],
-theme: {
-  screens: {
-    sm: '480px',
-    md: '768px',
-    lg: '976px',
-    xl: '1440px',
-  },
-  extend: {
-    colors: {
-      brightRed: 'hsl(12, 88%, 59%)',
-      brightRedLight: 'hsl(12, 88%, 69%)',
-      brightRedSupLight: 'hsl(12, 88%, 95%)',
-      darkBlue: 'hsl(228, 39%, 23%)',
-      darkGrayishBlue: 'hsl(227, 12%, 61%)',
-      veryDarkBlue: 'hsl(233, 12%, 13%)',
-      veryPaleRed: 'hsl(13, 100%, 96%)',
-      veryLightGray: 'hsl(0, 0%, 98%)',
+  theme: {
+    extend: {
+        colors: {
+            'accent': {
+              'navy': {
+                200: "#535c68",
+                100: "#485468",
+                75: "#BFD2F2",
+                50: "#D7E3F7"
+              }
+            }
+          }
     },
   },
-},
-plugins: [],
-}
+  plugins: [],
+};
