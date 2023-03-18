7e6b6864022a07e747d298d1c96c20eb5ed77d30
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-18 10:36:38 +0530

    Update Content

--- testing-app/src/App.tsx
+++ testing-app/src/App.tsx
@@ -3,7 +3,10 @@
 import React, { Component } from "react";
 
 import "./App.css";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 export default class App extends Component {
   componentDidMount(): void {
@@ -13,7 +16,10 @@ export default class App extends Component {
   render(): React.ReactNode {
     return (
       <div className="App bg-accent-navy-200 min-h-screen">
-        <Home/>
+        <Home />
+        <About />
+        <Contact />
+        <Profile />
       </div>
     );
   }
--- testing-app/src/pages/Profile/Profile.tsx
+++ testing-app/src/pages/Profile/Profile.tsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Profile extends Component {
   render() {
     return (
-      <h1>Profile</h1>
+      <h1>Profile Page</h1>
     )
   }
 }
