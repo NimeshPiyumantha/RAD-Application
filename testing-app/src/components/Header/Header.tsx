0059d60ce879d4f79af441b2a0e076676a0c82ec
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-18 14:30:34 +0530

    update Content

--- testing-app/src/components/Header/Header.tsx
+++ testing-app/src/components/Header/Header.tsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <div>Header</div>
+      <div cl>
+        <h1>Header</h1>
+      </div>
     )
   }
 }
