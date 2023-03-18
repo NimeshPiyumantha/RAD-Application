1fade8c10c59ece587e914f90205a2458cffda7c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-18 10:24:16 +0530

    Refactor Home Page

--- testing-app/src/App.tsx
+++ testing-app/src/App.tsx
@@ -3,6 +3,7 @@
 import React, { Component } from "react";
 
 import "./App.css";
+import Home from "./pages/Home";
 
 export default class App extends Component {
   componentDidMount(): void {
@@ -12,12 +13,7 @@ export default class App extends Component {
   render(): React.ReactNode {
     return (
       <div className="App bg-accent-navy-200 min-h-screen">
-        <h1>H 1 Tag</h1>
-        <h2>H 2 Tag</h2>
-        <h3>H 3 Tag</h3>
-        <h4>H 4 Tag</h4>
-        <h5>H 5 Tag</h5>
-        <h6>H 6 Tag</h6>
+        <Home/>
       </div>
     );
   }
