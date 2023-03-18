99c634364de5f82cfd665e544c79d2785af6d220
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-18 13:31:06 +0530

    Extend Component<CounterProps, CounterState>

--- testing-app/src/components/Counter/Counter.tsx
+++ testing-app/src/components/Counter/Counter.tsx
@@ -1,15 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export default class Counter extends Component {
+type CounterProps = {};
+type CounterState = {
+  count: number;
+};
+export default class Counter extends Component<CounterProps, CounterState> {
+
   render() {
     return (
-        <div className=''>
-            <h1>Counter : <span>0</span></h1>
-            <div className='flex space-x-3 mt-5'>
-                <button className='border border-green-600 px-4 py-2 text-green-600 font-semibold text-lg rounded-xl hover:bg-green-600 hover:text-white hover:drop-shadow-lg'>Increase</button>
-                <button className='border border-red-600 px-4 py-2 text-red-600 font-semibold rounded-xl text-lg hover:bg-red-600 hover:text-white hover:drop-shadow-lg'>Decrease</button>
-            </div>
+      <div className="">
+        <h1>
+          Counter : <span></span>
+        </h1>
+        <div className="flex space-x-3 mt-5">
+          <button
+            className="border border-green-600 px-4 py-2 text-green-600 font-semibold text-lg rounded-xl hover:bg-green-600 hover:text-white hover:drop-shadow-lg"
+            
+          >
+            Increase
+          </button>
+          <button
+            className="border border-red-600 px-4 py-2 text-red-600 font-semibold rounded-xl text-lg hover:bg-red-600 hover:text-white hover:drop-shadow-lg"
+            
+          >
+            Decrease
+          </button>
         </div>
-    )
+      </div>
+    );
   }
 }
