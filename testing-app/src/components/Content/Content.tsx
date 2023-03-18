fae9d26c5646e056fa0c49ca3864d2d6c3ffd354
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-18 16:12:31 +0530

    Update Content

--- testing-app/src/components/Content/Content.tsx
+++ testing-app/src/components/Content/Content.tsx
@@ -1,9 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "../../pages/About";
+import Home from "../../pages/Home";
+import Profile from "../../pages/Profile";
 
 export default class Content extends Component {
   render() {
     return (
-      <div>Content</div>
-    )
+      <div className="mt-20">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Content />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+        </Routes>
+      </div>
+    );
   }
 }
