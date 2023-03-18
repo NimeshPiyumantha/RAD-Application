1fd24dd5e1ef1af44de86ac3edaa71f6225b0f19
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-18 11:28:11 +0530

    Update Content

--- testing-app/src/components/Counter/Counter.tsx
+++ testing-app/src/components/Counter/Counter.tsx
@@ -6,8 +6,8 @@ export default class Counter extends Component {
         <div className=''>
             <h1>Counter : <span>0</span></h1>
             <div className='flex space-x-3 mt-5'>
-                <button className='border border-green-600 px-4 py-2 text-green-600 font-semibold rounded-xl hover:bg-green-600 hover:text-white'>Increase</button>
-                <button className='border border-red-600 px-4 py-2 text-red-600 font-semibold rounded-xl hover:bg-red-600 hover:text-white'>Decrease</button>
+                <button className='border border-green-600 px-4 py-2 text-green-600 font-semibold text-lg rounded-xl hover:bg-green-600 hover:text-white'>Increase</button>
+                <button className='border border-red-600 px-4 py-2 text-red-600 font-semibold rounded-xl text-lg hover:bg-red-600 hover:text-white'>Decrease</button>
             </div>
         </div>
     )
