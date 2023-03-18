eafced66eaf3c9476cc2feb96b23a66e209d670d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-18 13:47:59 +0530

    update Content

--- testing-app/src/components/Counter/Counter.tsx
+++ testing-app/src/components/Counter/Counter.tsx
@@ -28,14 +28,14 @@ export default class Counter extends Component<CounterProps, CounterState> {
         </h1>
         <div className="flex space-x-3 mt-5">
           <button
-            disabled={this.state.count === 10 ? true : false}
+            disabled={this.state.count === 10 ? true : false}//Increase count limit
             className="border border-green-600 px-4 py-2 text-green-600 font-semibold text-lg rounded-xl hover:bg-green-600 hover:text-white hover:drop-shadow-lg"
             onClick={this.increaseCount}
           >
             Increase
           </button>
           <button
-            disabled={this.state.count === 0 ? true : false}
+            disabled={this.state.count === 0 ? true : false}//Decrease count Limit
             className="border border-red-600 px-4 py-2 text-red-600 font-semibold rounded-xl text-lg hover:bg-red-600 hover:text-white hover:drop-shadow-lg"
             onClick={this.decreaseCount}
           >
