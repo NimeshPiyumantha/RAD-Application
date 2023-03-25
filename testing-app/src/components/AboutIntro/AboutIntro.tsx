0b8237e65bee2277793b9517d349c1ccab4dbf53
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-25 13:51:15 +0530

    Update Project

--- testing-app/src/components/AboutIntro/AboutIntro.tsx
+++ testing-app/src/components/AboutIntro/AboutIntro.tsx
@@ -1,9 +1,31 @@
-import React, { Component } from 'react'
+import { Component } from "react";
 
 export default class AboutIntro extends Component {
   render() {
     return (
-      <div>AboutIntro</div>
-    )
+      <div className="text-center text-slate-900 p-6">
+        <h1 className="mt-6">About Us</h1>
+        <section className="w-full flex justify-center">
+          <p className="p-6 w-1/2 text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+            voluptatum molestiae! Modi, doloribus fugit! Eius, odio!
+            Necessitatibus repellendus eos magnam blanditiis aperiam nulla
+            similique asperiores excepturi, voluptatibus minima eligendi quia.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure neque
+            minus nostrum deleniti eligendi officia nobis aliquam? Eligendi
+            laudantium aspernatur tempora, deserunt nulla molestias, assumenda
+            minima, aut necessitatibus vitae maiores. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Odio, voluptatum molestiae! Modi,
+            doloribus fugit! Eius, odio! Necessitatibus repellendus eos magnam
+            blanditiis aperiam nulla similique asperiores excepturi,
+            voluptatibus minima eligendi quia. Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Iure neque minus nostrum deleniti
+            eligendi officia nobis aliquam? Eligendi laudantium aspernatur
+            tempora, deserunt nulla molestias, assumenda minima, aut
+            necessitatibus vitae maiores.
+          </p>
+        </section>
+      </div>
+    );
   }
 }
