e0deee9f9e55e8b1ddc79284077bb22533c0099b
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-25 13:52:37 +0530

    Update Project

--- testing-app/src/components/CardContainer/CardContainer.tsx
+++ testing-app/src/components/CardContainer/CardContainer.tsx
@@ -1,9 +1,102 @@
-import React, { Component } from 'react'
+import { type } from "os";
+import React, { Component } from "react";
+import Card from "../Card/Card";
 
-export default class CardContainer extends Component {
+type CardContainerProps = {};
+
+type CardContainerState = {
+  cardList: CardContainerDetails[];
+};
+
+type CardContainerDetails = {
+  id: string;
+  title: string;
+  description: string;
+};
+
+export default class CardContainer extends Component<
+  CardContainerProps,
+  CardContainerState
+> {
+  constructor(props: CardContainerDetails) {
+    super(props);
+    this.state = {
+      cardList: [
+        {
+          id: "1",
+          title: "Card 1",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maximemaiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,impedit dolorem enim minus ullam pariatur rem in laborum sit quas.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,sed quo. Dolor maxime excepturi, temporibus magni tempore doloremvitae explicabo cupiditate, recusandae optio dolores reiciendistempora neque ratione cum nostrum?",
+        },
+        {
+          id: "2",
+          title: "Card 2",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maximemaiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,impedit dolorem enim minus ullam pariatur rem in laborum sit quas.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,sed quo. Dolor maxime excepturi, temporibus magni tempore doloremvitae explicabo cupiditate, recusandae optio dolores reiciendistempora neque ratione cum nostrum?",
+        },
+        {
+          id: "3",
+          title: "Card 3",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maximemaiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,impedit dolorem enim minus ullam pariatur rem in laborum sit quas.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,sed quo. Dolor maxime excepturi, temporibus magni tempore doloremvitae explicabo cupiditate, recusandae optio dolores reiciendistempora neque ratione cum nostrum?",
+        },
+        {
+          id: "4",
+          title: "Card 4",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maximemaiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,impedit dolorem enim minus ullam pariatur rem in laborum sit quas.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,sed quo. Dolor maxime excepturi, temporibus magni tempore doloremvitae explicabo cupiditate, recusandae optio dolores reiciendistempora neque ratione cum nostrum?",
+        },
+      ],
+    };
+  }
+
   render() {
     return (
-      <div>CardContainer</div>
-    )
+      <div className="flex justify-center gap-6 p-4">
+        {this.state.cardList.map((card) => (
+          <Card title={card.title} description={card.description} />
+        ))}
+        {/* <Card
+          title="Card 1"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maxime
+          maiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,
+          impedit dolorem enim minus ullam pariatur rem in laborum sit quas.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,
+          sed quo. Dolor maxime excepturi, temporibus magni tempore dolorem
+          vitae explicabo cupiditate, recusandae optio dolores reiciendis
+          tempora neque ratione cum nostrum?"
+        />
+        <Card
+          title="Card 2"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maxime
+          maiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,
+          impedit dolorem enim minus ullam pariatur rem in laborum sit quas.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,
+          sed quo. Dolor maxime excepturi, temporibus magni tempore dolorem
+          vitae explicabo cupiditate, recusandae optio dolores reiciendis
+          tempora neque ratione cum nostrum?"
+        />
+        <Card
+          title="Card 3"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maxime
+          maiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,
+          impedit dolorem enim minus ullam pariatur rem in laborum sit quas.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,
+          sed quo. Dolor maxime excepturi, temporibus magni tempore dolorem
+          vitae explicabo cupiditate, recusandae optio dolores reiciendis
+          tempora neque ratione cum nostrum?"
+        />
+        <Card
+          title="Card 4"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maxime
+          maiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,
+          impedit dolorem enim minus ullam pariatur rem in laborum sit quas.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,
+          sed quo. Dolor maxime excepturi, temporibus magni tempore dolorem
+          vitae explicabo cupiditate, recusandae optio dolores reiciendis
+          tempora neque ratione cum nostrum?"
+        /> */}
+      </div>
+    );
   }
 }
