1d2c3f182051bfd74bb85498d42db74289d9f547
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-25 11:40:18 +0530

    Update Project

--- testing-app/src/components/Post/Post.tsx
+++ testing-app/src/components/Post/Post.tsx
@@ -1,4 +1,3 @@
-import { type } from 'os';
 import React, { Component } from 'react'
 import Tag from '../Tag';
 
--- testing-app/src/pages/Home/Home.tsx
+++ testing-app/src/pages/Home/Home.tsx
@@ -6,7 +6,7 @@ export default class Home extends Component {
     return (
       <div className="p-6">
         <Post title="Nowty Pasan" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam vero amet, temporibus dolorum voluptates nisi delectus maiores quia dolorem aspernatur doloremque ipsam soluta accusamus. Asperiores harum corporis fuga. Fugiat." tags={["Intro","Description"]}/>
-        <Post title="Nowty Sadun" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam vero amet, temporibus dolorum voluptates nisi delectus maiores quia dolorem aspernatur doloremque ipsam soluta accusamus. Asperiores harum corporis fuga. Fugiat." tags={["Intro","Description"]}/>
+        <Post title="Nowty Sadun" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam vero amet, temporibus dolorum voluptates nisi delectus maiores quia dolorem aspernatur doloremque ipsam soluta accusamus. Asperiores harum corporis fuga. Fugiat." tags={["Intro","Commit"]}/>
       </div>
     );
   }
