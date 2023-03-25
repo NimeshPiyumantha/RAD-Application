9aed9b38678c3c719e409ecfe3c93478e1db8c5f
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-25 13:13:47 +0530

    Update Content

--- testing-app/src/pages/Home/Home.tsx
+++ testing-app/src/pages/Home/Home.tsx
@@ -1,21 +1,72 @@
 import React, { Component } from "react";
 import Post from "../../components/Post/Post";
 
-type PostDetails={
-  id:string;
-  title:string;
-  description:React.ReactNode;
-  hourCount?:string;
-  lectureName:string;
-  tags:string[];
+type PostDetails = {
+  id: string;
+  title: string;
+  description: React.ReactNode;
+  hoursCount?: number;
+  lecturerName?: string;
+  tags: string[];
 };
+type HomeProps={};
+type HomeState={
+  postList:PostDetails[];
+};
+export default class Home extends Component <HomeProps,HomeState>{
+  constructor(props: HomeProps) {
+    super(props);
+    this.state = {
+      postList: [
+        {
+          id: "1",
+          title: "Post 1",
+          description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse.",
+            hoursCount: 10,
+          tags: ["ui", "ux"],
+        },
+        {
+          id: "2",
+          title: "Post 2",
+          description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse.",
+            hoursCount: 10,
+          tags: ["ui", "ux"],
+        },
+        {
+          id: "3",
+          title: "Post 3",
+          description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse.",
+            hoursCount: 10,
+          tags: ["ui", "ux"],
+        },
+        {
+          id: "4",
+          title: "Post 4",
+          description:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse.",
+            hoursCount: 10,
+          tags: ["ui", "ux"],
+        },
+      ],
+    };
+  }
 
-export default class Home extends Component {
-  render() {
-    return (
+  render(){
+    return(
       <div className="p-6">
-        <Post title="Nowty Pasan" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam vero amet, temporibus dolorum voluptates nisi delectus maiores quia dolorem aspernatur doloremque ipsam soluta accusamus. Asperiores harum corporis fuga. Fugiat." tags={["Intro","Description"]}/>
-        <Post title="Nowty Sadun" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam vero amet, temporibus dolorum voluptates nisi delectus maiores quia dolorem aspernatur doloremque ipsam soluta accusamus. Asperiores harum corporis fuga. Fugiat." tags={["Intro","Commit"]}/>
+        {this.state.postList.map((post, index) =>(
+          <Post  
+          key={post.id}
+          title={post.title}
+          description={post.description}
+          hoursCount={post.hoursCount}
+          lecturerName={post.lecturerName}
+          tags={post.tags}
+          />
+        ))}
       </div>
     );
   }
