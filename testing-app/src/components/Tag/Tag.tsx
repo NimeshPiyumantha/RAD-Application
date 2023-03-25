c4455eaef04e5a4ac1a2b4751f5405cae8172153
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-25 10:19:49 +0530

    Update Project

--- testing-app/src/components/Tag/Tag.tsx
+++ testing-app/src/components/Tag/Tag.tsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 
-type tagProps={
+type TagProps={
     text:string;
 };
 
 type TagState={};
-export default class Tag extends Component {
+export default class Tag extends Component <TagProps,TagState>{
   render() {
     return (
-      <div>Tag</div>
+        <span className='px-6 py-2 border border-slate-400 rounded'>
+        <h6>{this.props.text}</h6>
+    </span>
     )
   }
 }
