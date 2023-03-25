4cf525d2c2a8da27f6b65f1c6db194912b5d26c8
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-25 15:30:42 +0530

    Update Content

---
+++ testing-app/src/components/ContactIntro/ContactIntro.tsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class ContactIntro extends Component {
+    render() {
+        return (
+          <div className="text-center text-slate-900 p-6">
+            <h1 className="mt-6">Contact Us</h1>
+            <section className="w-full flex justify-center">
+              <p className="p-6 w-3/5 text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio,
+                voluptatum molestiae! Modi, doloribus fugit! Eius, odio!
+                Necessitatibus repellendus eos magnam blanditiis aperiam nulla
+                similique asperiores excepturi, voluptatibus minima eligendi quia.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure neque
+                minus nostrum deleniti eligendi officia nobis aliquam? Eligendi
+                laudantium aspernatur tempora, deserunt nulla molestias, assumenda
+                minima, aut necessitatibus vitae maiores. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Odio, voluptatum molestiae! Modi,
+                doloribus fugit! Eius, odio! Necessitatibus repellendus eos magnam
+                blanditiis aperiam nulla similique asperiores excepturi,
+                voluptatibus minima eligendi quia. Lorem ipsum, dolor sit amet
+                consectetur adipisicing elit. Iure neque minus nostrum deleniti
+                eligendi officia nobis aliquam? Eligendi laudantium aspernatur
+                tempora, deserunt nulla molestias, assumenda minima, aut
+                necessitatibus vitae maiores.
+              </p>
+            </section>
+          </div>
+        );
+      }
+    }
--- testing-app/src/pages/Contact/Contact.tsx
+++ testing-app/src/pages/Contact/Contact.tsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class Contact extends Component {
   render() {
     return (
-      <div className="">
+      <div className="p-6">
         <h1>Contact Page</h1>
       </div>
     );
