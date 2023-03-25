4d229bbb26a9b07652e607142da6cdd47e3c283e
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-25 10:29:58 +0530

    Update Project

--- testing-app/src/pages/Home/Home.tsx
+++ testing-app/src/pages/Home/Home.tsx
@@ -5,8 +5,8 @@ export default class Home extends Component {
   render() {
     return (
       <div className="p-6">
-        <Post title="Nowty Pasan" description="I am good Boy" tags={["Intro","Description"]}/>
-        <Post title="Nowty Sadun" description="I am good Boy" tags={["Intro","Description"]}/>
+        <Post title="Nowty Pasan" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam vero amet, temporibus dolorum voluptates nisi delectus maiores quia dolorem aspernatur doloremque ipsam soluta accusamus. Asperiores harum corporis fuga. Fugiat." tags={["Intro","Description"]}/>
+        <Post title="Nowty Sadun" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ullam vero amet, temporibus dolorum voluptates nisi delectus maiores quia dolorem aspernatur doloremque ipsam soluta accusamus. Asperiores harum corporis fuga. Fugiat." tags={["Intro","Description"]}/>
       </div>
     );
   }
