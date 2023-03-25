7d3d0cafbcc309a3d7a781f8c9f79a1f809f1409
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-25 13:56:53 +0530

    Update Project

--- testing-app/src/components/CardContainer/CardContainer.tsx
+++ testing-app/src/components/CardContainer/CardContainer.tsx
@@ -55,46 +55,6 @@ export default class CardContainer extends Component<
         {this.state.cardList.map((card) => (
           <Card title={card.title} description={card.description} />
         ))}
-        {/* <Card
-          title="Card 1"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maxime
-          maiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,
-          impedit dolorem enim minus ullam pariatur rem in laborum sit quas.
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,
-          sed quo. Dolor maxime excepturi, temporibus magni tempore dolorem
-          vitae explicabo cupiditate, recusandae optio dolores reiciendis
-          tempora neque ratione cum nostrum?"
-        />
-        <Card
-          title="Card 2"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maxime
-          maiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,
-          impedit dolorem enim minus ullam pariatur rem in laborum sit quas.
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,
-          sed quo. Dolor maxime excepturi, temporibus magni tempore dolorem
-          vitae explicabo cupiditate, recusandae optio dolores reiciendis
-          tempora neque ratione cum nostrum?"
-        />
-        <Card
-          title="Card 3"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maxime
-          maiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,
-          impedit dolorem enim minus ullam pariatur rem in laborum sit quas.
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,
-          sed quo. Dolor maxime excepturi, temporibus magni tempore dolorem
-          vitae explicabo cupiditate, recusandae optio dolores reiciendis
-          tempora neque ratione cum nostrum?"
-        />
-        <Card
-          title="Card 4"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maxime
-          maiores ab, nobis quam sapiente eaque reiciendis voluptatem quasi,
-          impedit dolorem enim minus ullam pariatur rem in laborum sit quas.
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi,
-          sed quo. Dolor maxime excepturi, temporibus magni tempore dolorem
-          vitae explicabo cupiditate, recusandae optio dolores reiciendis
-          tempora neque ratione cum nostrum?"
-        /> */}
       </div>
     );
   }
