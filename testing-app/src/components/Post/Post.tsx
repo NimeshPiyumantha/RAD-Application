ad4d9f7b886e8486c10ac7810066841a95dd0610
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-25 09:47:08 +0530

    Update Project

--- testing-app/src/components/Post/Post.tsx
+++ testing-app/src/components/Post/Post.tsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
 
+type PostProps ={
+    title:string;
+    description:string;
+    tags:string[];
+};
+
 export default class Post extends Component {
   render() {
     return (
-      <div>Post</div>
+      <div className='p-6 border border-slate-400 w-full rounded mt-2'>
+        <h3 className='text-center'>Post 1</h3>
+        <p>hiddhddd</p>
+        <span className='w-full mt-3 flex space-x-3'>
+            <span className='px-6 py-2 border border-slate-400 rounded'>
+                <h6>Tag 1</h6>
+            </span>
+            <span className='px-6 py-2 border border-slate-400 rounded'>
+                <h6>Tag 1</h6>
+            </span>
+            <span className='px-6 py-2 border border-slate-400 rounded'>
+                <h6>Tag 1</h6>
+            </span>
+            <span className='px-6 py-2 border border-slate-400 rounded'>
+                <h6>Tag 1</h6>
+            </span>
+        </span>
+      </div>
     )
   }
 }
--- testing-app/src/pages/Home/Home.tsx
+++ testing-app/src/pages/Home/Home.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import Post from "../../components/Post/Post";
 
 export default class Home extends Component {
   render() {
     return (
       <div className="p-6">
-        <h1>Home Page</h1>
+        <Post/>
+        <Post/>
+        <Post/>
       </div>
     );
   }
