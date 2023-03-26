3cf793bcf1611f085e7afc8aab9f9c917defcd52
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-26 09:31:33 +0530

    Update Content

--- testing-app/src/pages/Profile/Profile.tsx
+++ testing-app/src/pages/Profile/Profile.tsx
@@ -1,11 +1,15 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import Header from "../../components/Header";
 
 export default class Profile extends Component {
-  render() {
-    return (
-      <div className="">
-        <h1>Profile Page</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <>
+                <Header/>
+                <div className="p-6 mt-20">
+                    <h1>Profile Page</h1>
+                </div>
+            </>
+        );
+    }
 }
