1083a59a44bd01d90b627c56065c53243cc7922c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-03-26 09:21:32 +0530

    Update Content

--- testing-app/src/components/Tag/Tag.tsx
+++ testing-app/src/components/Tag/Tag.tsx
@@ -8,8 +8,8 @@ type TagState={};
 export default class Tag extends Component <TagProps,TagState>{
   render() {
     return (
-        <span className='inline-block m-3 px-6 py-2 border border-slate-400 rounded'>
-        <h6>{this.props.text}</h6>
+        <span className="px-6 py-2 border border-slate-400 rounded">
+            <h6>{this.props.text}</h6>
     </span>
     )
   }
