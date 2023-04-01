c77065bf791375e15b93a1709aa8482484ca36cc
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-01 21:33:32 +0530

    Update Content

--- testing-app/src/components/Counter/Counter.tsx
+++ testing-app/src/components/Counter/Counter.tsx
@@ -14,26 +14,29 @@ export default class Counter extends Component<CounterProps, CounterState> {
 
   // React DOM Life Cycle Methods
 
-  /*Create*/ 
-  componentDidMount(): void {//DOM එකට inject වෙන වෙලාවට වැඩ කරන්නේ
-    console.log("Console Did Mount");
+  /*Create*/
+  componentDidMount(): void {
+    //DOM එකට inject වෙන වෙලාවට වැඩ කරන්නේ
+    console.log("Console Did Mount....");
   }
 
-    /*Update*/ 
-  componentDidUpdate(): void {//DOM එක Update වෙන time එක
-    console.log("Console Did Update");
+  /*Update*/
+  componentDidUpdate(): void {
+    //DOM එක Update වෙන time එක
+    console.log("Console Did Update....");
   }
 
-    /*Destory*/  //method එකක් නෑ.
+  /*Destory*/ //method එකක් නෑ.
 
-
-  componentWillUnmount(): void {//DOM එකෙන්  Did mount වෙන වෙලාවට කලිම් මොකක් හරි කර ගන්න ඔන්නම් use කරන්නෙ.(Hooking Process එක වගේ)
+  componentWillUnmount(): void {
+    //DOM එකෙන්  Did mount වෙන වෙලාවට කලිම් මොකක් හරි කර ගන්න ඔන්නම් use කරන්නෙ.(Hooking Process එක වගේ)
     console.log("Console Did Unmount");
   }
 
- componentDidCatch(): void {//DOM එකෙන් Did mount වෙන වෙලාවට කලිම් handle නොකරපු execption තිබුනොත් එක handle කරගන්න use කරන method එක 
-  console.log("Console Did Catch");
- }
+  componentDidCatch(): void {
+    //DOM එකෙන් Did mount වෙන වෙලාවට කලිම් handle නොකරපු execption තිබුනොත් එක handle කරගන්න use කරන method එක
+    console.log("Console Did Catch");
+  }
 
   increaseCount = () => {
     //Increase Count by one
