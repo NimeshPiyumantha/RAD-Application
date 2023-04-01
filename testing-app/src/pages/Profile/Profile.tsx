07cb8e00f84a4bf083c193c71048a378ec44874b
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-01 20:39:23 +0530

    Add Component param

--- testing-app/src/pages/Profile/Profile.tsx
+++ testing-app/src/pages/Profile/Profile.tsx
@@ -9,7 +9,7 @@ type ProfileState = {
   postList: PostDetails[];
 };
 
-export default class Home extends Component {
+export default class Home extends Component<ProfileProps, ProfileState> {
   render() {
     return <div className="pt-6 pr-28 pl-28 pb-6"></div>;
   }
