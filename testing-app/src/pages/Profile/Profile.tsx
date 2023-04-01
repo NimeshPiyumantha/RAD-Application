ebae7d4f3a9eed4726fa0951736f6a503730910e
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-01 20:38:25 +0530

    Update Content

--- testing-app/src/pages/Profile/Profile.tsx
+++ testing-app/src/pages/Profile/Profile.tsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Post from "../../components/Post";
 import Header from "../../components/Header";
+import { PostDetails } from "../../types/PostDetails";
+
+type ProfileProps = {};
 
 export default class Home extends Component {
   render() {
