1db09cd42454033a18039574cee5e71d1343b092
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-01 21:24:54 +0530

    Update Content

--- testing-app/src/pages/Profile/Profile.tsx
+++ testing-app/src/pages/Profile/Profile.tsx
@@ -229,14 +229,14 @@ export default class Profile extends Component<ProfileProps, ProfileState> {
 
           <Divider className="!my-5" />
 
-          {this.state.postList.map((post, index) => (
+          {this.state.postList.map((post) => (
             <Post
               key={post.id}
               title={post.title}
               description={post.description}
+              tags={post.tags}
               hoursCount={post.hoursCount}
               lecturerName={post.lecturerName}
-              tags={post.tags}
             />
           ))}
         </div>
