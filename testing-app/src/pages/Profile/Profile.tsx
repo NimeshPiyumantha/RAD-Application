ae629adca3def459405badcc03201e04e0c16a0f
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-01 21:07:37 +0530

    Add Create New post

--- testing-app/src/pages/Profile/Profile.tsx
+++ testing-app/src/pages/Profile/Profile.tsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import Post from "../../components/Post";
 import Header from "../../components/Header";
 import { PostDetails } from "../../types/PostDetails";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import PostAddIcon from "@mui/icons-material/PostAdd";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
+import { Divider, TextField } from "@mui/material";
 
 type ProfileProps = {};
 
@@ -89,6 +93,18 @@ export default class Profile extends Component<ProfileProps, ProfileState> {
       <>
         <Header />
         <div className="py-6 px-32">
+          <div className="w-full flex flex-col space-y-1">
+            <>
+              <div className="cursor-pointer w-full p-4 bg-accent-navy-200 text-white rounded flex justify-between items-center">
+                <h6>Create New Post</h6>
+                <AddCircleIcon />
+              </div>
+              <div className="cursor-pointer p-8 bg-white rounded text-slate-400 flex justify-center items-center space-x-3 border border-slate-400">
+                <PostAddIcon />
+                <h6>Your New Post</h6>
+              </div>
+            </>
+          </div>
           {this.state.postList.map((post, index) => (
             <Post
               key={post.id}
