757ffe19094423fc19eb791f339f93b263a33792
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-30 08:08:09 +0530

    Update class compotent to funtionl components

--- testing-app/src/pages/Home/Home.tsx
+++ testing-app/src/pages/Home/Home.tsx
@@ -1,106 +1,45 @@
-import React, { Component } from "react";
-import Post from "../../components/Post";
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
+import Post from "../../components/Post";
+import "./home.css";
 import { PostDetails } from "../../types/PostDetails";
+import api from "../../axios";
 
-type HomeProps = {};
+const Home = () => {
+  const [postList, setPostList] = useState<PostDetails[]>([]);
 
-type HomeState = {
-  postList: PostDetails[];
+  useEffect(() => {
+    retrieveAllPosts();
+  }, []);
+
+  const retrieveAllPosts = () => {
+    api
+      .get("post")
+      .then((res) => {
+        console.log(res);
+        setPostList(res.data.responseData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="p-6">
+      {postList.map((post) => (
+          <Post
+            key={post._id}
+            _id={post._id}
+            title={post.title}
+            description={post.description}
+            hoursCount={post.hoursCount}
+            lecturerName={post.lecturerName}
+            tags={post.tags}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
-
-export default class Home extends Component<HomeProps, HomeState> {
-  constructor(props: HomeProps) {
-    super(props);
-    this.state = {
-      postList: [
-        {
-          id: "1",
-          title: "Lecture Day 01",
-          description: (
-            <p>
-              Lecture D01 - Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit. Eligendi alias laborum accusantium iusto molestias. Magnam
-              nisi atque, nihil, ex quaerat quam velit deserunt consequatur
-              soluta pariatur cumque accusamus consequuntur. Quidem! Lorem ipsum
-              dolor sit amet consectetur, adipisicing elit. Eligendi alias
-              laborum accusantium iusto molestias. Magnam nisi atque, nihil, ex
-              quaerat quam velit deserunt consequatur soluta pariatur cumque
-              accusamus consequuntur. Quidem! Lorem ipsum dolor sit amet
-              consectetur, adipisicing elit. Eligendi alias laborum accusantium
-              iusto molestias. Magnam nisi atque, nihil, ex quaerat quam velit
-              deserunt consequatur soluta pariatur cumque accusamus
-              consequuntur. Quidem!
-            </p>
-          ),
-          hoursCount: 8,
-          tags: ["intro", "typescript"],
-        },
-        {
-          id: "2",
-          title: "Lecture Day 02",
-          description: (
-            <p>
-              Lecture D02 - Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit. Eligendi alias laborum accusantium iusto molestias. Magnam
-              nisi atque, nihil, ex quaerat quam velit deserunt consequatur
-              soluta pariatur cumque accusamus consequuntur. Quidem! Lorem ipsum
-              dolor sit amet consectetur, adipisicing elit. Eligendi alias
-              laborum accusantium iusto molestias. Magnam nisi atque, nihil, ex
-              quaerat quam velit deserunt consequatur soluta pariatur cumque
-              accusamus consequuntur. Quidem! Lorem ipsum dolor sit amet
-              consectetur, adipisicing elit. Eligendi alias laborum accusantium
-              iusto molestias. Magnam nisi atque, nihil, ex quaerat quam velit
-              deserunt consequatur soluta pariatur cumque accusamus
-              consequuntur. Quidem!
-            </p>
-          ),
-          hoursCount: 6,
-          lecturerName: "Chanu",
-          tags: ["react", "tailwind"],
-        },
-        {
-          id: "3",
-          title: "Lecture Day 03",
-          description: (
-            <p>
-              Lecture D03 - Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit. Eligendi alias laborum accusantium iusto molestias. Magnam
-              nisi atque, nihil, ex quaerat quam velit deserunt consequatur
-              soluta pariatur cumque accusamus consequuntur. Quidem! Lorem ipsum
-              dolor sit amet consectetur, adipisicing elit. Eligendi alias
-              laborum accusantium iusto molestias. Magnam nisi atque, nihil, ex
-              quaerat quam velit deserunt consequatur soluta pariatur cumque
-              accusamus consequuntur. Quidem! Lorem ipsum dolor sit amet
-              consectetur, adipisicing elit. Eligendi alias laborum accusantium
-              iusto molestias. Magnam nisi atque, nihil, ex quaerat quam velit
-              deserunt consequatur soluta pariatur cumque accusamus
-              consequuntur. Quidem!
-            </p>
-          ),
-          tags: ["Lifecycle", "rounting", "structure"],
-        },
-      ],
-    };
-  }
-
-  render() {
-    return (
-      <>
-        <Header />
-        <div className="p-6">
-          {this.state.postList.map((post, index) => (
-            <Post
-              key={post.id}
-              title={post.title}
-              description={post.description}
-              hoursCount={post.hoursCount}
-              lecturerName={post.lecturerName}
-              tags={post.tags}
-            />
-          ))}
-        </div>
-      </>
-    );
-  }
-}
