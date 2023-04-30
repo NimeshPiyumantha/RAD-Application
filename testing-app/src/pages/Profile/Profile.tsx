5c431c35430b72240e1dfce7dce10357e972e8c8
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-30 07:45:52 +0530

    Profile Constroctor

--- testing-app/src/pages/Profile/Profile.tsx
+++ testing-app/src/pages/Profile/Profile.tsx
@@ -30,74 +30,8 @@ export default class Profile extends Component<ProfileProps, ProfileState> {
       lecturerName: "",
       tagString: "",
       isClickedCreateNewPost: false,
-      postList: [
-        {
-          id: "1",
-          title: "Lecture Day 01",
-          description: (
-            <p>
-              Lecture D01 - Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit. Eligendi alias laborum accusantium iusto molestias. Magnam
-              nisi atque, nihil, ex quaerat quam velit deserunt consequatur
-              soluta pariatur cumque accusamus consequuntur. Quidem! Lorem ipsum
-              dolor sit amet consectetur, adipisicing elit. Eligendi alias
-              laborum accusantium iusto molestias. Magnam nisi atque, nihil, ex
-              quaerat quam velit deserunt consequatur soluta pariatur cumque
-              accusamus consequuntur. Quidem! Lorem ipsum dolor sit amet
-              consectetur, adipisicing elit. Eligendi alias laborum accusantium
-              iusto molestias. Magnam nisi atque, nihil, ex quaerat quam velit
-              deserunt consequatur soluta pariatur cumque accusamus
-              consequuntur. Quidem!
-            </p>
-          ),
-          hoursCount: 8,
-          tags: ["intro", "typescript"],
-        },
-        {
-          id: "2",
-          title: "Lecture Day 02",
-          description: (
-            <p>
-              Lecture D02 - Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit. Eligendi alias laborum accusantium iusto molestias. Magnam
-              nisi atque, nihil, ex quaerat quam velit deserunt consequatur
-              soluta pariatur cumque accusamus consequuntur. Quidem! Lorem ipsum
-              dolor sit amet consectetur, adipisicing elit. Eligendi alias
-              laborum accusantium iusto molestias. Magnam nisi atque, nihil, ex
-              quaerat quam velit deserunt consequatur soluta pariatur cumque
-              accusamus consequuntur. Quidem! Lorem ipsum dolor sit amet
-              consectetur, adipisicing elit. Eligendi alias laborum accusantium
-              iusto molestias. Magnam nisi atque, nihil, ex quaerat quam velit
-              deserunt consequatur soluta pariatur cumque accusamus
-              consequuntur. Quidem!
-            </p>
-          ),
-          hoursCount: 6,
-          lecturerName: "Chanu",
-          tags: ["react", "tailwind"],
-        },
-        {
-          id: "3",
-          title: "Lecture Day 03",
-          description: (
-            <p>
-              Lecture D03 - Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit. Eligendi alias laborum accusantium iusto molestias. Magnam
-              nisi atque, nihil, ex quaerat quam velit deserunt consequatur
-              soluta pariatur cumque accusamus consequuntur. Quidem! Lorem ipsum
-              dolor sit amet consectetur, adipisicing elit. Eligendi alias
-              laborum accusantium iusto molestias. Magnam nisi atque, nihil, ex
-              quaerat quam velit deserunt consequatur soluta pariatur cumque
-              accusamus consequuntur. Quidem! Lorem ipsum dolor sit amet
-              consectetur, adipisicing elit. Eligendi alias laborum accusantium
-              iusto molestias. Magnam nisi atque, nihil, ex quaerat quam velit
-              deserunt consequatur soluta pariatur cumque accusamus
-              consequuntur. Quidem!
-            </p>
-          ),
-          tags: ["Lifecycle", "rounting", "structure"],
-        },
-      ],
+      categoryName: "",
+      postList: [],
     };
   }
 
